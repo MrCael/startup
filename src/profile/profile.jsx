d051622f51028f8d2f62f3fa06a6ef26625f6050
Cael Erickson <cerickson@q90.com>
2025-10-04 22:14:54 -0600

    Fixed css problems in profile component

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -16,9 +16,9 @@ export function Profile() {
                             </tr>
                             <tr>
                                 <td className="d-flex flex-row">
-                                    <p>Profile Image: </p>
+                                    <p className="profile-img-row-style">Profile Image: </p>
                                     <img src="cael.jpg" alt="Profile Image" className="img-fluid img-thumbnail profile-pic" />
-                                    <button className="profile-btn btn btn-secondary">Edit Profile Image</button>
+                                    <button className="profile-img-row-style btn btn-secondary">Edit Profile Image</button>
                                 </td>
                             </tr>
                             <tr>
@@ -68,61 +68,63 @@ export function Profile() {
                     <button className="btn btn-secondary">Edit Shippign Information</button>
                     <span className="note">This is a database placeholder</span>
                     <table className="table">
-                        <tr>
-                            <td colspan="2">
-                                <h1>Shipping Information</h1>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p>Name: Cael Erickson</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p>Address Line 1: 603 N 100 W</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p>Address Line 2: Apt 8</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p>City: Provo</p>
-                            </td>
-                            <td>
-                                <p>State: UT</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p>Zip Code: 84601</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <h1>Billing Information</h1>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p>Card Number: **** **** **** 1234 <button className="btn btn-secondary">Show Card Number</button></p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <p>Name on Card: Cael Erickson</p>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <button className="btn btn-secondary profile-button">Add New Payment Method</button>
-                                <button className="btn btn-secondary profile-button">View Purchase History</button>
-                                <span className="note">These are React placeholders</span>
-                            </td>
-                        </tr>
+                        <tbody>
+                            <tr>
+                                <td colspan="2">
+                                    <h1>Shipping Information</h1>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <p>Name: Cael Erickson</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <p>Address Line 1: 603 N 100 W</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <p>Address Line 2: Apt 8</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <p>City: Provo</p>
+                                </td>
+                                <td>
+                                    <p>State: UT</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <p>Zip Code: 84601</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <h1>Billing Information</h1>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <p>Card Number: **** **** **** 1234 <button className="btn btn-secondary">Show Card Number</button></p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <p>Name on Card: Cael Erickson</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    <button className="btn btn-secondary profile-button">Add New Payment Method</button>
+                                    <button className="btn btn-secondary profile-button">View Purchase History</button>
+                                    <span className="note">These are React placeholders</span>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
