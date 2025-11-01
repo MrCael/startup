45eee93b0da1633db45d7f45a8132e7e5808c4eb
Cael Erickson <cse37@byu.edu>
2025-11-01 03:17:55 -0600

    Implimented get request to get mesurements and displayed them for edit with a map method

--- src/editUser/measurementInfo.jsx
+++ src/editUser/measurementInfo.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 
-function ViewMeasurements() { /// Probably impliment this in a list or something ///
+import { useEffect } from "react";
+
+function ViewMeasurements({ measurements }) {
     return (
         <>
             <tr>
@@ -120,196 +122,82 @@ function ViewMeasurements() { /// Probably impliment this in a list or something
     );
 }
 
-function EditMeasurements() { /// Probably impliment this in a list or something ///
-    return (
-        <>
-            <tr>
-                <td>
-                    <p>1</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>1</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>2</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>2</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>3</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>3</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>4</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>4</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>5</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>5</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>5a</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>5a</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>6</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>6</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>6a</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>6a</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>7</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>7</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>7a</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>7a</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>8</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>8</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>8a</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>8a</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>9</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>9</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <p>10</p>
-                    <input type="text" className="form-control" />
-                </td>
-                <td>
-                    <p>10</p>
-                    <input type="text" className="form-control" />
-                </td>
-            </tr>
-        </>
-    );
+function EditMeasurements({ measurements }) {
+        return (
+            <div className="d-flex flex-row">
+                <div style={{ margin: "10px" }}>
+                    {Object.entries(measurements.left).map(([key, value]) => (
+                        <div>
+                            <p style={{ paddingLeft: "10px" }}>{key}</p>
+                            <input type="text" className="form-control" defaultValue={value || ""} />
+                        </div>
+                    ))}
+                </div>
+                <div style={{ margin: "10px" }}>
+                    {Object.entries(measurements.left).map(([key, value]) => (
+                        <div>
+                            <p style={{ paddingLeft: "10px" }}>{key}</p>
+                            <input type="text" className="form-control" defaultValue={value || ""} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
 }
 
 export function MeasurementInfo({ from }) {
     // If `method` is `true` then the page is in view mode. If it is `false` then is it in edit mode
     const [method, setMethod] = React.useState(from != "login");
+    const [measurements, setMeasurements] = React.useState(null);
 
     async function saveInfo() {
         const response = await fetch("/api/user/measurementInfo", {
-            // code
+            method: "PATCH",
+            body: measurements,
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            credentials: "include"
         });
+        
+        const body = await response.json();
+        console.log(body.msg);
     }
+
+    useEffect(() => {
+        async function getMeasurements() {
+            const response = await fetch("/api/user/measurementInfo", {
+                credentials: "include"
+            });
+
+
+            const body = await response.json();
+            setMeasurements(body.measurements);
+            console.log(measurements);
+        }
+
+        getMeasurements();
+    }, []);
 
     return (
         <main>
             <div className="d-flex flex-column justify-content-center align-div">
-                <table style={{marginBottom: "10px" }}>
-                    <tbody>
-                        <tr>
-                            <td colSpan="2">
-                                <h1 className="centered">Measurements</h1>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2">
-                                <img src="foot_measurements.png" alt="Foot Measurements" className="img-fluid img-thumbnail measurement-pic" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="centered">
-                                <h3>Left</h3>
-                            </td>
-                            <td className="centered">
-                                <h3>Right</h3>
-                            </td>
-                        </tr>
-                        {method && <ViewMeasurements />}
-                        {!method && <EditMeasurements />}
-                        <tr>
-                            <td colSpan="2">
-                                <Button className="btn btn-primary form-control" onClick={() => setMethod(!method)}>{method ? "Edit" : "Save"}</Button>
-                                {!method && <Button className="btn btn-secondary form-control" onClick={() => setMethod(true)}>Cancel</Button>}
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div className="centered">
+                    <h1 className="centered">Measurements</h1>
+                    <img src="foot_measurements.png" alt="Foot Measurements" className="img-fluid img-thumbnail measurement-pic" />
+                    <div className="d-flex flex-row justify-content-around" style={{ marginTop: "10px" }}>
+                        <h3 className="centered">Left</h3>
+                        <h3 className="centered">Right</h3>
+                    </div>
+                </div>
+                <div>
+                    {method && measurements && <ViewMeasurements measurements={measurements} />}
+                    {!method && measurements && <EditMeasurements measurements={measurements} />}
+                </div>
+                <div className="centered">
+                    <Button className="btn btn-primary form-control" onClick={() => setMethod(!method)}>{method ? "Edit" : "Save"}</Button>
+                    {!method && <Button className="btn btn-secondary form-control" onClick={() => setMethod(true)}>Cancel</Button>}
+                </div>
             </div>
         </main>
     );
